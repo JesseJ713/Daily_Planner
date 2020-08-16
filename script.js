6d99448cc92292b686e147dd5c70c1318c4df663
JCJ <jesse.jackson.c@gmail.com>
2020-08-16 18:39:53 -0500

    I've created the skeleton of the program. Need to work on storage manipulation, accurate representation of time, and layout mechanics.

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Setting a global array for hours referencing the 24 hour method
+  var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-    //Establishing current date from DOM
-    // var currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+  // Establishing current date from DOM
+  // var currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
 
-    setInterval(function() {
-        var currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
-        $("#currentDay").html(currentDate);
-    }, 100);
+  setInterval(function () {
+    var currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+    $("#currentDay").html(currentDate);
+  }, 100);
 
+  // This is running the function through each hour in the global variable and formatting it into the hour and AM PM
+  hours.forEach(function (time, index) {
 
+    var time = [];
+    time.push(moment().hour(time).format("h A"));
+    // console.log(parseInt(time));
+    // console.log(moment().hour());
+
+    // Declaring new element variables
+    var divEl = $("<div>");
+    var spanEl = $("<span>");
+    var inputEl = $("<input>");
+    var buttonEl = $("<button>");
+
+    // Appending new elements onto the page
+    $(".container").append(divEl);
+    // $(".container").addClass("row");
+
+    $(divEl).addClass("row");
+    // $(divEl).attr("data-val", index);
+    $(divEl).append(spanEl);
+    $(spanEl).text(time);
+    // $(spanEl).addClass("time-block");
+    $(spanEl).addClass("hour");
+    $(divEl).append(inputEl);
+    $(inputEl).addClass("description");
+    $(divEl).append(buttonEl);
+    $(buttonEl).addClass("saveBtn");
+    $(buttonEl).text("save placeholder");
+
+  });
 });
